91c857d11ddb4853a0408fd5d697de35f1bfc585
Caio <caioalmeidaacl@gmail.com>
2024-05-20 08:55:46 -0300

    feat: Add Header and Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import clsx from "clsx";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import { createClient } from "@/prismicio";
 
+import Header from "@/app/components/Header";
+import Footer from "./components/Footer";
 
 const nunito = Nunito({
   subsets: ['latin'], // especifica o conjunto de caracteres que serão carregados.
@@ -20,14 +22,14 @@ const nunitoSans = Nunito_Sans({
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
 
-  const page = await client.getSingle("settings"); 
+  const settings = await client.getSingle("settings"); 
       
   return {
-    title: page.data.site_title || "Flowrise fallback",
-    description: page.data.meta_description || "Flowrise is the relaxing app for you",
+    title: settings.data.site_title || "Flowrise fallback",
+    description: settings.data.meta_description || "Flowrise is the relaxing app for you",
 
     openGraph: {
-      images: [page.data.og_image.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
   }
 }
@@ -41,9 +43,9 @@ export default function RootLayout({
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
 
-        <header>Header!</header>
+        <Header />
         {children}
-        <footer>Fotter!</footer>
+        <Footer /> 
         
       
       </body>
