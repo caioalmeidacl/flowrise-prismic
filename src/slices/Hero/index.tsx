64c1bd51d4a8d988c177aaf56f886163b984c9f4
Caio <caioalmeidaacl@gmail.com>
2024-05-26 10:51:02 -0300

    feat: Add snapshot to Hero Slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,7 +8,7 @@ import Heading from "@/app/components/Heading";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
-    <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">
+    <Heading as="h1" size="xl" className="md:mb-8 mb-4 first:mt-0 last:mb-0">
       {children}
   </Heading>
   ), 
